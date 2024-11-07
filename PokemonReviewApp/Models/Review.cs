b9229c339ff2dc478f4dfed19a49b74bf5a33359
aaron <lucidlear@gmail.com>
2024-11-07 19:19:26 +1100

    3 - One to Many Relationships

--- PokemonReviewApp/Models/Review.cs
+++ PokemonReviewApp/Models/Review.cs
@@ -6,5 +6,8 @@
         public string Title { get; set; }
 
         public string Text { get; set; }
+
+        public Reviewer Reviewer { get; set; }
+        public Pokemon Pokemon { get; set; }
     }
 }
