a334fba36870c32008d20ca91afeb726a8eeb672
aaron <lucidlear@gmail.com>
2024-11-27 13:10:25 +1100

    9 - POST and Create Methods P1

--- PokemonReviewApp/Interfaces/ICategoryRepository.cs
+++ PokemonReviewApp/Interfaces/ICategoryRepository.cs
@@ -4,9 +4,13 @@ namespace PokemonReviewApp.Interfaces
 {
     public interface ICategoryRepository
     {
+        // read members
         ICollection<Category> GetCategories();
         Category GetCategory(int id);
         ICollection<Pokemon> GetPokemonByCategory(int categoryId);
         bool CategoryExists(int id);
+        // create members
+        bool CreateCategory(Category category);
+        bool Save();
     }
 }
--- PokemonReviewApp/Interfaces/ICountryRepository.cs
+++ PokemonReviewApp/Interfaces/ICountryRepository.cs
@@ -10,5 +10,8 @@ namespace PokemonReviewApp.Interfaces
 
         ICollection<Owner> GetOwnersFromACountry(int countryId);
         bool CountryExists(int id);
+
+        bool CreateCountry(Country country);
+        bool Save();
     }
 }
